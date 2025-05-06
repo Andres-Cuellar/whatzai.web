bed86949b3e8986a09c5e3d3f8365be95f259b87
Andres Cuellar <afca133@gmail.com>
2025-05-05 22:37:09 -0500

    se agrega webhook 2

--- src/components/DemoForm.tsx
+++ src/components/DemoForm.tsx
@@ -5,12 +5,16 @@ import { selectedPlan } from "../context/SelectedPlanContext";
 export default function DemoForm() {
   const plan = useStore(selectedPlan);
 
+  const webhook = import.meta.env.PUBLIC_WEBHOOK_URL;
+
+  console.log("webhook", webhook);
+
   function handleChange(event: Event) {
     const target = event.target as HTMLSelectElement;
     selectedPlan.set(target.value || null); // Actualiza el store si el usuario cambia el select
   }
 
-  console.log('plan', selectedPlan);
+  console.log("plan", selectedPlan);
 
   const handleSubmitForm = async (event: Event) => {
     event.preventDefault();
@@ -19,17 +23,13 @@ export default function DemoForm() {
     const data = Object.fromEntries(formData.entries());
 
     try {
-      const response = await fetch(
-        "https://apiai.glumpstudio.com/webhook/4b8596f8-c80c-4306-8693-c89dbd9e2321",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          // mode: 'no-cors',
-          body: JSON.stringify(data),
-        }
-      );
+      const response = await fetch(webhook, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
       console.log("response", response);
 
@@ -131,7 +131,10 @@ export default function DemoForm() {
         </div>
 
         <div class="mb-6">
-          <label htmlFor="plan" class="block text-sm font-medium text-gray-700 mb-2">
+          <label
+            htmlFor="plan"
+            class="block text-sm font-medium text-gray-700 mb-2"
+          >
             Plan (opcional)
           </label>
           <select
@@ -165,8 +168,7 @@ export default function DemoForm() {
         </div>
 
         <div class="flex flex-col md:flex-row gap-4">
-          <button type="submit" class="btn btn-secondary flex-1"
-          >
+          <button type="submit" class="btn btn-secondary flex-1">
             Solicitar demo gratuita
           </button>
           <a href="#planes" class="btn btn-primary flex-1">
@@ -175,7 +177,5 @@ export default function DemoForm() {
         </div>
       </form>
     </div>
-
-
   );
 }
